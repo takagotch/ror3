f6b1898464d1a38d723f03045b9f6a44657e84f3
takagotch <dyaccb@gmail.com>
2018-01-09 00:02:21 +0900

    update()

--- find_ex.rb
+++ find_ex.rb
@@ -24,9 +24,9 @@ cart.save
 
 
 #find_by_sql()
-orders = LineItem.find_by_sql("" +
-			      "" +
-			      "")
+orders = LineItem.find_by_sql("select line_items.* from line_items, orders " +
+			      " where order_id = orders.id " +
+			      " and orders.name = 'xxx' ")
 #attributes()
 orders = Order.find_by_sql("select name, pay_type from orders")
 first = orders[0]
@@ -35,24 +35,59 @@ p first.attribute_names
 p first.attribute_present?("address")
 
 #find_by_sql()
-items = LineItem.find_by_sql(",
-			     " +
-			    "" +
-			    "" +
-			    "" +
-			    "" +
-			    "")
+items = LineItem.find_by_sql("select *,
+			      products.price as unit_price, " +
+			      " quantity*products.price as total_price, " +
+			      " products.title as title " +
+			      " products.title as title " +
+			    " from lile_items, products " +
+			    " where line_items.product_id = products.id ")
 li = items[o]
-puts "#{}: #{}x#{} => #{}"
-Order.find_by_sql([
+puts "#{li.quantity}: #{li.unit_price}x#{li.unit_price} => #{li.total_price}"
+Order.find_by_sql(["select * from orders where amount > ?",
 	params:[:amount])
 
 #reload()
 stock = Market.find_by_ticker("xxx")
 loop do
-	puts ""
+	puts "Price = #{stock.price}"
 	sleep 60
 	stock.reload
 end
+
+
+order = Order.find(123)
+order.name = "Taka"
+order.save
+#find_by_sql("select * from * where id=123")
+orders = Order.find_by_sql("select id, name, pay_type from orders where id=123")
+first = orders[0]
+first.name = "Wilma"
+first.save
+#update_attributes()
+order = Order.find(123)
+order.update_attirbutes(:name, "Taka", email: "taka@ex.com")
+#update_attributes()
+def save_after_edit
+	order = Order.find(params[:id])
+	if order.update_attributes(params[:order])
+		reditect_to action: :index
+	else
+		render action: :edit
+end
+
+#update()
+order = Order.update(12, name: "Taka", email: "taka@ex.com")
+#update_all()
+result = Product.update_all("price = 1.1*price", "title like '%xxx%'")
+#save!, create!
+if order.save
+else
+end
+
+
+
+
+
 
 

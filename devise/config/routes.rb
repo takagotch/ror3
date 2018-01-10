0c12971e61451b22231b98cb2519f7463421ecc3
takagotch <dyaccb@gmail.com>
2018-01-11 04:42:05 +0900

    root,devise_for,resources; get, put

---
+++ devise/config/routes.rb
@@ -0,0 +1,13 @@
+Nchak::Application.routes.draw do
+root :to => 'welcome#index'
+
+devise_for :users
+get 'tasks', :to => 'tasks#index', :as => :user_root
+
+resources :tasks, :only => [ :index, :create ] do
+put :finish, :on => :member
+put :unfinish, :on => :member
+get :done, :on => :collection
+end
+end
+

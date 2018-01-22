91302308e65c245fd77692ea48c163b2ac311e02
takagotch <dyaccb@gmail.com>
2018-01-23 05:05:34 +0900

    delivery_method_options

--- mailer/user_mailer.rb
+++ mailer/user_mailer.rb
@@ -1,3 +1,4 @@
+#
 class UserMailer < ApplicationMailer
   default form: 'notifications@ex.com'
 
@@ -8,7 +9,7 @@ class UserMailer < ApplicationMailer
   end
 end
 
-
+#after_action, before_action, around_action
 class UserMailer < ApplicationMailer
   after_action :set_delivery_options,
 	       :prevent_delivery_to_guests,
@@ -46,5 +47,35 @@ class UserMailer < ApplicationMailer
   end
 end
 
+#recieve
+#/bin/rails runner 'UserMailer.receive(STDIN.read)'
+class UserMailer < ApplicationMailer
+  def receive(email)
+    page = Page.find_by(address: email.to.first)
+    page.emails.create(
+      subject: email.subject,
+      body: email.body
+    )
 
+    if email.has_attachemetns?
+      email.attachements.each do |attachment|
+        page.attachments.create({
+	  file: attachment,
+	  description: email.subject
+	})
+      end
+    end
+  end
+end
+
+#:content_type
+#text/plain #default
+class UserMailer < ApplicationMailer
+  def welcome_email(user, email_body)
+    mail(to: user.emial,
+	body: email_body,
+	content_type: "text/html",
+	subject: "")
+  end
+end
 

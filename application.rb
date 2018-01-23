6dd41268f47ec80c05358433c7d7cff5f04ba1e4
takagotch <dyaccb@gmail.com>
2018-01-24 02:52:48 +0900

    Rails.configuration.payment['merchant_id']

--- application.rb
+++ application.rb
@@ -1,2 +1,20 @@
 config.active_record.observers = :order_observer, :audit_obserber
 
+config.x.payment_processing.shedule = :daily
+config.x.payment_processing.retries = 3
+config.super_debugger = true
+
+Rails.configuration.x.payment_processing.schedule #=> :daily
+Rails.configuration.x.payment_processing.retries  #=> 3
+Rails.configuration.x.payment_rpcessing.not_set   #=> nil
+Rails.configuration.x.super_debugger              #=> true
+
+module MyApp
+  class Applicatin < Rails::Application
+    config.payment = config_for(:payment)
+  end
+end
+
+Rails.configuration.payment['merchant_id'] #=> production_merchant_id or development_merchant_id
+
+
